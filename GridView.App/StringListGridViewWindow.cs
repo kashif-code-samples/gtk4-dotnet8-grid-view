25505c1cc2bc56b9c703c477b43e4bac2885ad72
Kashif Soofi <kashif.soofi+git@gmail.com>
2024-02-17 18:35:07 +0000

    Display string in GridView

---
+++ GridView.App/StringListGridViewWindow.cs
@@ -0,0 +1,58 @@
+﻿namespace GridView.App;
+
+using Gtk;
+using static Gtk.SignalListItemFactory;
+
+public class StringListGridViewWindow : Window
+{
+    public StringListGridViewWindow()
+        : base()
+    {
+        Title = "Gtk::GridView (Gio::ListStore)";
+        SetDefaultSize(400, 400);
+
+        var stringList = StringList.New(["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]);
+        var selectionModel = NoSelection.New(stringList);
+        var listItemFactory = SignalListItemFactory.New();
+        listItemFactory.OnSetup += SetupSignalHandler;
+        listItemFactory.OnBind += BindSignalHandler;
+
+        var gridView = GridView.New(selectionModel, listItemFactory);
+
+        var scrolledWindow = ScrolledWindow.New();
+        scrolledWindow.Child = gridView;
+        Child = scrolledWindow;
+    }
+
+    private void SetupSignalHandler(SignalListItemFactory sender, SetupSignalArgs args)
+    {
+        var listItem = args.Object as ListItem;
+        if (listItem is null)
+        {
+            return;
+        }
+
+        var label = Label.New(null);
+        label.WidthRequest = 100;
+        label.HeightRequest = 100;
+        listItem.Child = label;
+    }
+
+    private void BindSignalHandler(SignalListItemFactory sender, BindSignalArgs args)
+    {
+        var listItem = args.Object as ListItem;
+        if (listItem is null)
+        {
+            return;
+        }
+
+        var label = listItem.Child as Label;
+        if (label is null)
+        {
+            return;
+        }
+
+        var item = listItem.Item as StringObject;
+        label.SetText(item?.String ?? "NOT FOUND");
+    }
+}
